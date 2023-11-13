10a9f2efccbc9c7cd535300de374bc4bf77e6a88
opokuamaning <princeopokuamaning@gmail.com>
2023-11-12 17:00:50 -0800

    made changes to _printf.c

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 #include <stdarg.h>
-
+#include <stdio.h>
 /**
  * _printf - produces output according to a format
  * @format: identifier for all initial strings
@@ -14,46 +14,60 @@ int _printf(const char *format, ...)
 	/* Initialise the argument list with the format */
 	va_start(args, format);
 	/* This count tracks the number of characters we print */
-	for (; *format; format++)
+	
+	while(*format)
 	{
-		if(*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
+		if(*format == '%')
 		{
-			int num = va_arg(args, int);
-			int temp = num;
-
-			/* Count the number of digits */
-			int digits = (num == 0) ? 1 : 0;
-			
-			while(temp != 0)
-			{
-				temp /= 10;
-				digits++;
-			}
-
-			/* Convert each digit to a character adn wrtie to stdout */
-			temp = num;
-
-			for (; digits > 0; digits--)
-			{
-				char digit = '0' + temp % 10;
-				write(1, &digit, 1);
-				count++;
-				temp /= 10;
-			}
-			/* Move to the next charachter after the spcifier */
 			format++;
-		}
-		else if(*format == '%' && *(format + 1) == 'n')
-		{
-			int *nptr = va_arg(args, int *);
-			*nptr = count;
-			format += 2;
+			if(*format == 'c')
+			{
+				int character = va_arg(args, int);
+				write(1, &character, 1);
+				count++;
+			}
+			else if(*format == 's')
+			{
+				char *str = va_arg(args, char *);
+				while(*str)
+				{
+					write(1, str, 1);
+					count++;
+					str++;
+				}
+			}
+			else if(*format == '%')
+			{
+				write(1, "%", 1);
+				{
+					count++;
+				}
+			}
+			else if(*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(args, int);
+				char buffer[12];
+				int digits = sprintf(buffer, "%d", num);
+				write(1, buffer, digits);
+				count += digits;
+			}
+			else
+			{
+				write(1, "%", 1);
+				if(*format)
+				{
+					write(1, format, 1);
+					count += 2;
+				}
+			}
+
 		}
 		else
 		{
 			write(1, format, 1);
 			count++;
 		}
+		format++;
 	}
 	va_end(args);
 	return (count);
